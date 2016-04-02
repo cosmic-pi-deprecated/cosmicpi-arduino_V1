d2ef2ff385aef6c39d922ee492323f53da366508
julian lewis <lewis.julian@gmail.com>
2016-04-02 14:10:21 +0200

    Got rid of unwanted code, tidied up and tested OK
    Signed-off-by: julian lewis <lewis.julian@gmail.com>

--- pi-server/pi-server.py
+++ pi-server/pi-server.py
@@ -1,5 +1,5 @@
-#!      /usr/bin/python
-#       coding: utf8
+#!	/usr/bin/python
+#	coding: utf8
 
 """
 Handle UDP packets from the cosmic pi and log then
@@ -23,60 +23,60 @@ from optparse import OptionParser
 
 class KeyBoard(object):
 
-        def __init__(self):
-                self.fd = sys.stdin.fileno()
+	def __init__(self):
+		self.fd = sys.stdin.fileno()
 
-        def echo_off(self):
-                self.oldterm = termios.tcgetattr(self.fd)
-                self.newattr = termios.tcgetattr(self.fd)
-                self.newattr[3] = self.newattr[3] & ~termios.ICANON & ~termios.ECHO
-                termios.tcsetattr(self.fd, termios.TCSANOW, self.newattr)
-                self.oldflags = fcntl.fcntl(self.fd, fcntl.F_GETFL)
-                fcntl.fcntl(self.fd, fcntl.F_SETFL, self.oldflags | os.O_NONBLOCK)
+	def echo_off(self):
+		self.oldterm = termios.tcgetattr(self.fd)
+		self.newattr = termios.tcgetattr(self.fd)
+		self.newattr[3] = self.newattr[3] & ~termios.ICANON & ~termios.ECHO
+		termios.tcsetattr(self.fd, termios.TCSANOW, self.newattr)
+		self.oldflags = fcntl.fcntl(self.fd, fcntl.F_GETFL)
+		fcntl.fcntl(self.fd, fcntl.F_SETFL, self.oldflags | os.O_NONBLOCK)
 
-        def echo_on(self):
-                termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.oldterm)
-                fcntl.fcntl(self.fd, fcntl.F_SETFL, self.oldflags)
+	def echo_on(self):
+		termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.oldterm)
+		fcntl.fcntl(self.fd, fcntl.F_SETFL, self.oldflags)
 
-        def test_input(self):
-                res = False
+	def test_input(self):
+		res = False
 
-                try:
-                        c = sys.stdin.read(1)
-                        if c == '>':
-                                res = True
-                except IOError: pass
-                return res
+		try:
+			c = sys.stdin.read(1)
+			if c == '>':
+				res = True
+		except IOError: pass
+		return res
 
 class Socket_io(object):
 
-        def __init__(self,ipport):
-                try:
-                        self.sik = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                        self.sik.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-                        self.sik.setblocking(0)
-                        self.sik.bind(("",ipport))
+	def __init__(self,ipport):
+		try:
+			self.sik = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			self.sik.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+			self.sik.setblocking(0)
+			self.sik.bind(("",ipport))
 
-                except Exception, e:
-                        msg = "Exception: Can't open Socket: %s" % (e)
-                        print "Sending OFF:%s" % msg
-                        udpflg = False
+		except Exception, e:
+			msg = "Exception: Can't open Socket: %s" % (e)
+			print msg
+			sys.exit(1)
 
-        def recv_event_pkt(self):
-                try:
-                        available = select.select([self.sik], [], [], 1)
-                        if available[0]:
-                                recv = self.sik.recvfrom(2048)
-                                return recv
+	def recv_event_pkt(self):
+		try:
+			available = select.select([self.sik], [], [], 1)
+			if available[0]:
+				recv = self.sik.recvfrom(2048)
+				return recv
 
-                except Exception, e:
-                        msg = "Exception: Can't recvfrom: %s" % (e)
-                        print msg
+		except Exception, e:
+			msg = "Exception: Can't recvfrom: %s" % (e)
+			print msg
 
-                return ["",""]
+		return ["",""]
 
-        def close(self):
-                self.sik.close()
+	def close(self):
+		self.sik.close()
 
 # This is the event object, it builds a dictionary from incomming jsom strings 
 # and provides access to the dictionary entries containing the data for each field.
@@ -106,9 +106,6 @@ class Event(object):
 				"ACL":self.ACL, "AOL":self.AOL, "LOC":self.LOC, "TIM":self.TIM, "STS":self.STS,
 				"EVT":self.EVT, "DAT":self.DAT }
 
-		self.oetm = "f"
-		self.ovib = "i"
-
 	# Convert the incomming json strings into entries in the dictionary 
 
 	def parse(self, line):					# parse the incomming json strings from arduino
@@ -121,63 +118,6 @@ class Event(object):
 			
 		except Exception, e:
 			pass					# Didnt understand, throw it away
-
-	def extract(self, entry):
-		if self.recd.has_key(entry):
-			if entry is "DAT":
-				self.recd["DAT"] = time.asctime(time.gmtime(time.time()))
-			nstr = "{\'%s\':%s}" % (entry,str(self.recd[entry]))
-			return nstr
-		else:
-			return ""
-
-	# build weather, cosmic ray and vibration event strings suitable to be sent over the network to server
-	# these strings are self describing json format for easy decoding at the server end
-
-	def get_weather(self):
-
-		# Build a list of dictionary entries as a string
-
-		self.weather =	"[" + self.extract("HTU") + \
-				"," + self.extract("BMP") + \
-				"," + self.extract("LOC") + \
-				"," + self.extract("TIM") + \
-				"," + self.extract("DAT") + \
-				"]"
-		return self.weather
-
-	def get_event(self):
-		if self.oetm == self.recd["EVT"]["Etm"]:
-			return ""
-
-		self.oetm = self.recd["EVT"]["Etm"]
-
-		self.evt =	"[" + self.extract("EVT") + \
-				"," + self.extract("BMP") + \
-				"," + self.extract("ACL") + \
-				"," + self.extract("MAG") + \
-				"," + self.extract("HTU") + \
-				"," + self.extract("STS") + \
-				"," + self.extract("LOC") + \
-				"," + self.extract("TIM") + \
-				"," + self.extract("DAT") + \
-				"]\n"
-		return self.evt
-
-	def get_vibration(self):
-		if self.ovib == self.recd["VIB"]["Vcn"]:
-			return ""
-
-		self.ovib = self.recd["VIB"]["Vcn"]
-
-		self.vib =	"[" + self.extract("VIB") + \
-				"," + self.extract("ACL") + \
-				"," + self.extract("MAG") + \
-				"," + self.extract("LOC") + \
-				"," + self.extract("TIM") + \
-				"," + self.extract("DAT") + \
-				"]\n"
-		return self.vib
 
 	# Here we just return dictionaries
 
@@ -208,58 +148,61 @@ class Event(object):
 	def get_evt(self):
 		return self.recd["EVT"]
 
+	def get_dat(self):
+		return self.recd["DAT"]
+
 def main():
-        use = "Usage: %prog [--port=4901 --odir=/tmp]"
-        parser = OptionParser(usage=use, version="cosmic_pi_server version 1.0")
-        parser.add_option("-p", "--port",  help="Server portnumber", dest="ipport", type="int", default="15443")
-        parser.add_option("-d", "--debug", help="Debug Option", dest="debug", default=False, action="store_true")
-        parser.add_option("-o", "--odir",  help="Path to log directory", dest="logdir", default="/tmp")
-        parser.add_option("-n", "--nolog", help="Event Logging", dest="logflg", default=True, action="store_false")
+	use = "Usage: %prog [--port=4901 --odir=/tmp]"
+	parser = OptionParser(usage=use, version="cosmic_pi_server version 1.0")
+	parser.add_option("-p", "--port",  help="Server portnumber", dest="ipport", type="int", default="15443")
+	parser.add_option("-d", "--debug", help="Debug Option", dest="debug", default=False, action="store_true")
+	parser.add_option("-o", "--odir",  help="Path to log directory", dest="logdir", default="/tmp")
+	parser.add_option("-n", "--nolog", help="Event Logging", dest="logflg", default=True, action="store_false")
 
-        options, args = parser.parse_args()
+	options, args = parser.parse_args()
 
-        ipport = options.ipport
-        logdir = options.logdir
-        debug  = options.debug
-        logflg = options.logflg
+	ipport = options.ipport
+	logdir = options.logdir
+	debug  = options.debug
+	logflg = options.logflg
 
-        print ""
+	print ""
 	print "cosmic_pi server running, hit '>' for commands\n"
 
-        print "options (Server Port number)     port:%d" % ipport
-        print "options (Logging directory)      odir:%s" % logdir
-        print "options (Event logging)          log: %s" % logflg
+	print "options (Server Port number)	port:%d" % ipport
+	print "options (Logging directory)	odir:%s" % logdir
+	print "options (Event logging)		log: %s" % logflg
 
-        now = time.asctime(time.localtime(time.time()))
-        now = now.replace(" ","-")
-        now = now.replace(":","-")
+	now = time.asctime(time.localtime(time.time()))
+	now = now.replace(" ","-")
+	now = now.replace(":","-")
 
-        lgf = "%s/cosmicpi-logs/%s.log" % (logdir,now)
-        dir = os.path.dirname(lgf)
-        if not os.path.exists(dir):
-                os.makedirs(dir)
-        try:
-                log = open(lgf, "w");
-        except Exception, e:
-                msg = "Exception: Cant open log file: %s" % (e)
-                print "Fatal: %s" % msg
-                sys.exit(1)
+	lgf = "%s/cosmicpi-logs/%s.log" % (logdir,now)
+	dir = os.path.dirname(lgf)
+	if not os.path.exists(dir):
+		os.makedirs(dir)
+	try:
+		log = open(lgf, "w");
+	except Exception, e:
+		msg = "Exception: Cant open log file: %s" % (e)
+		print "Fatal: %s" % msg
+		sys.exit(1)
 
-        if options.debug:
-                print "\n"
-                print "Log file is: %s" % lgf
+	if options.debug:
+		print "\n"
+		print "Log file is: %s" % lgf
 
-        kbrd = KeyBoard()
-        kbrd.echo_off()
+	kbrd = KeyBoard()
+	kbrd.echo_off()
 
-        sio = Socket_io(ipport)
+	sio = Socket_io(ipport)
 
 	evt = Event()
 
-        try:
-                while(True):
+	try:
+		while(True):
 
-                        recv = sio.recv_event_pkt()
+			recv = sio.recv_event_pkt()
 			if len(recv[0]):
 
 				if debug:
@@ -273,43 +216,47 @@ def main():
 
 					if nstr[0].find("EVT") != -1:
 						evd = evt.get_evt()
+						tim = evt.get_tim()
+						dat = evt.get_dat()
 						print
 						print "Cosmic Event..: Evt:%s Frq:%s Tks:%s Etm:%s" % (evd["Evt"],evd["Frq"],evd["Tks"],evd["Etm"])
-						print "Adc...........: %s" % (str(evd["Adc"])) 
+						print "Adc[[Ch0][Ch1]: Adc:%s" % (str(evd["Adc"]))
+						print "Time..........: Upt:%s Sec:%s" % (tim["Upt"],tim["Sec"])
+						print "Date..........: Dat:%s" % (dat["Dat"])
 
 					elif nstr[0].find("VIB") != -1:
+						mag = evt.get_mag()
+						vib = evt.get_vib()
+						tim = evt.get_tim()
+						acl = evt.get_acl()
 						print
-                                                vib = evt.get_vib()
-                                                print "Vibration.....: Vax:%s Vcn:%s " % (vib["Vax"],vib["Vcn"])
-                                                tim = evt.get_tim()
-                                                print "Time..........: Sec:%s" % (tim["Sec"])
-                                                acl = evt.get_acl()
-                                                print "Accelarometer.: Acx:%s Acy:%s Acz:%s" % (acl["Acx"],acl["Acy"],acl["Acz"])
-                                                mag = evt.get_mag()
-                                                print "Magnatometer..: Mgx:%s Mgy:%s Mgz:%s" % (mag["Mgx"],mag["Mgy"],mag["Mgz"])
+						print "Vibration.....: Vax:%s Vcn:%s " % (vib["Vax"],vib["Vcn"])
+						print "Time..........: Sec:%s" % (tim["Sec"])
+						print "Accelarometer.: Acx:%s Acy:%s Acz:%s" % (acl["Acx"],acl["Acy"],acl["Acz"])
+						print "Magnatometer..: Mgx:%s Mgy:%s Mgz:%s" % (mag["Mgx"],mag["Mgy"],mag["Mgz"])
 
 					elif nstr[0].find("HTU") != -1:
-                                                tim = evt.get_tim()
-                                                bmp = evt.get_bmp()
-                                                htu = evt.get_htu()
-                                                loc = evt.get_loc()
+						tim = evt.get_tim()
+						bmp = evt.get_bmp()
+						htu = evt.get_htu()
+						loc = evt.get_loc()
 						print
-                                                print "Barometer.....: Tmb:%s Prs:%s Alb:%s" % (bmp["Tmb"],bmp["Prs"],bmp["Alb"])
-                                                print "Humidity......: Tmh:%s Hum:%s Alt:%s" % (htu["Tmh"],htu["Hum"],loc["Alt"])
-                                                print "Time..........: Sec:%s\n" % (tim["Sec"])
+						print "Barometer.....: Tmb:%s Prs:%s Alb:%s" % (bmp["Tmb"],bmp["Prs"],bmp["Alb"])
+						print "Humidity......: Tmh:%s Hum:%s Alt:%s" % (htu["Tmh"],htu["Hum"],loc["Alt"])
+						print "Time..........: Sec:%s\n" % (tim["Sec"])
 
 				if logflg:
 					line = "%s - %s" % (str(recv[0]),str(recv[1]))
 					log.write(line)
 					log.write("\n\n")
 
-                        if kbrd.test_input():
-                                kbrd.echo_on()
-                                print "\n"
-                                cmd = raw_input(">")
+			if kbrd.test_input():
+				kbrd.echo_on()
+				print "\n"
+				cmd = raw_input(">")
 
-                                if cmd.find("q") != -1:
-                                        break
+				if cmd.find("q") != -1:
+					break
 
 				kbrd.echo_off()
 
@@ -318,17 +265,17 @@ def main():
 			sys.stdout.write(s)
 			sys.stdout.flush()
 
-        except Exception, e:
-                msg = "Exception: main: %s" % (e)
-                print "Fatal: %s" % msg
+	except Exception, e:
+		msg = "Exception: main: %s" % (e)
+		print "Fatal: %s" % msg
 
-        finally:
-                kbrd.echo_on()
-                print "Quitting ..."
-                log.close()
-                sio.close()
-                time.sleep(1)
-                sys.exit(0)
+	finally:
+		kbrd.echo_on()
+		print "Quitting ..."
+		log.close()
+		sio.close()
+		time.sleep(1)
+		sys.exit(0)
 
 if __name__ == '__main__':
 
